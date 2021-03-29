9c1726a7608e5c7d52d093f6bd99a3987b9d3399
bemin.shaker <beminshaker1@outlook.com>
2021-03-28 20:48:24 -0400

    upload files

---
+++ HW 6/max_shaker.py
@@ -0,0 +1,10 @@
+
+def max_name_len(firstname, lastname):
+    print("The largest value of the length of my firstname  and  lastname  is",
+          max(len(firstname), len(lastname)))
+
+
+first = "Bemin"
+last = "Shaker"
+
+max_name_len(first, last)
---
+++ HW 6/prod_shaker.py
@@ -0,0 +1,10 @@
+
+def prod_len_name(firstname, lastname):
+    print("The product of the value of the lengths of  my  firstname  and  lastname is", len(
+        firstname) * len(lastname))
+
+
+first = "Bemin"
+last = "Shaker"
+
+prod_len_name(first, last)
