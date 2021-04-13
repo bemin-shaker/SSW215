96547e6035d90057952c4afc80d7f3127890b526
bemin.shaker <beminshaker1@outlook.com>
2021-04-12 21:39:38 -0400

    HW 9

--- HW 9/worder_Shaker.py
+++ HW 9/worder_Shaker.py
@@ -1,7 +1,8 @@
-def word_order():
-    items = " apple, mango, carrot, apple, orange, mango, berry"
+def word_order(items):
     words = [word for word in items.split(",")]
     print(",".join(sorted(list(set(words)))))
 
 
-word_order()
+items = " apple, mango, carrot, apple, orange, mango, berry"
+
+word_order(items)
